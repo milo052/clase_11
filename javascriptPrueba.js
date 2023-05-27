50695d7e626c2d4da1da8c974172ef9bc6d8e760
milo052 <130940713+milo052@users.noreply.github.com>
2023-05-27 11:53:33 -0600

    clase_11
    esta es la clase numero 11

---
+++ javascriptPrueba.js
@@ -0,0 +1,29 @@
+var helado = 350;
+alert("su helado cuesta $350")
+alert("¿le quiere agregar otro producto?")
+
+
+var topping=prompt(" Ingrese el nombre de su Producto");
+var precio ; 
+var precioFinal; 
+
+if (topping == "Oreo"){
+ alert("se le agregaran $"+(precio = 10 ) + " al producto final");
+}
+else if(topping == "KitKat"){
+   alert("se le agregaran $"+(precio = 15 ) + " al producto final");
+}
+else if (topping == "Kinder"){
+   alert("se le agregaran $"+(precio = 25 ) + " al producto final");
+}
+ else if (alert(
+   "No tenemos este topping"))
+
+console.log(precios)
+   var precioFinal= helado+precio;
+
+
+alert ("El precio final es: "+(precioFinal));
+
+
+alert("muchas gracias")
